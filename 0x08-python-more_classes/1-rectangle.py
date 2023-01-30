832fb47e6195a03179f425ab2421f6bf436d8088
‘PachoIhirwe’ <alainpa18@gmail.com>
2023-01-30 02:38:08 -0800

    task 2 more classes py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,36 @@
 #!/usr/bin/python3
+"""Rectangle Class."""
 
 class Rectangle:
     """Defines the blueprint of a rectangle"""
+    def __init__(self, width=0, height=0):
+         """An object constructor method."""
+         self.__width = width
+         self.__height = height
 
-    pass
+    @property
+    def width(self):
+       """Gets the width private attribute value"""
+
+       return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
