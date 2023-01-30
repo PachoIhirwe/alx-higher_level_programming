c698a2f818c44e3e185c5f6bcf892abf34ff98f0
‘PachoIhirwe’ <alainpa18@gmail.com>
2023-01-30 07:17:42 -0800

    task 5 more classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -2,15 +2,23 @@
 
 class Rectangle:
     """Defines the blueprint of a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """An object constructor method."""
+
+
         self.__width = width
         self.__height = height
     @property
     def width(self):
+        """An object constructor method."""
+
         return self.__width
 
     @width.setter
     def width(self, value):
+
+        """An object constructor method."""
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -20,10 +28,15 @@ class Rectangle:
 
     @property
     def height(self):
+        """An object constructor method."""
+
         return self.__height
 
     @height.setter
     def height(self, value):
+
+        """Sets the height private attribute value."""
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -31,9 +44,12 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """A public object method."""
+
         return self.__width * self.__height
 
     def perimeter(self):
+        """A public object method."""
 
         if self.width == 0 or self.height == 0:
             return 0
